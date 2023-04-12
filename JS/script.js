90106150b0e528986c606654adae470687cd016d
lgustavobarre351 <rm99210@fiap.com.br>
2023-04-11 21:00:05 -0300

    teste de funçoes

---
+++ JS/script.js
@@ -0,0 +1,11 @@
+const divElement = document.getElementById("div-box")
+
+// divElement.addEventListener("click", ()=>{
+//     console.log(divElement);
+// });
+
+divElement.addEventListener("click", mostraElemento);
+
+function mostraElemento() {
+    console.log(divElement)
+}
