9873a3e66c16f6466ebf38a5fbc228e0aa85a1a4
lgustavobarre351 <rm99210@fiap.com.br>
2023-04-18 20:43:15 -0300

    testes css

--- JS/script.js
+++ JS/script.js
@@ -1,11 +1,32 @@
-const divElement = document.getElementById("div-box")
+// const divElement = document.getElementById("div-box")
 
-// divElement.addEventListener("click", ()=>{
-//     console.log(divElement);
-// });
+// // divElement.addEventListener("click", ()=>{
+// //     console.log(divElement);
+// // });
 
-divElement.addEventListener("click", mostraElemento);
+// divElement.addEventListener("click", mostraElemento);
 
-function mostraElemento() {
-    console.log(divElement)
+// function mostraElemento() {
+//     console.log(divElement)
+// }
+
+// function soma() {
+//     let n1 = 10
+//     let n2 = 5
+//     console.log(n1 + n2)
+// }
+
+//Recuperando elelemtos do html com quary
+
+// const liElement = document.querySelectorAll("li");
+
+//Convertendo notelist em array spread
+const arrayElements = [...document.querySelectorAll("li")];
+
+arrayElements.map(  (el,key)=>{
+
+    if (el.textContent.indexOf("item")){
+        el.textContent = `[ ${el.textContent} ]`;
 }
+});
+
