ba699689563e5a275b70c3fde4b7d9500c537e00
lgustavobarre351 <rm99210@fiap.com.br>
2023-04-25 20:35:29 -0300

    Adicionando e ditando os banners em css

--- JS/script.js
+++ JS/script.js
@@ -21,12 +21,65 @@
 // const liElement = document.querySelectorAll("li");
 
 //Convertendo notelist em array spread
-const arrayElements = [...document.querySelectorAll("li")];
+// const arrayElements = [...document.querySelectorAll("li")];
 
-arrayElements.map(  (el,key)=>{
+// arrayElements.map(  (el,key)=>{
 
-    if (el.textContent.indexOf("item")){
-        el.textContent = `[ ${el.textContent} ]`;
+//     if (el.textContent.indexOf("item")){
+//         el.textContent = `[ ${el.textContent} ]`;
+// }
+// });
+
+let tmp = 0;
+
+function mudaCor() {
+    let r = Math.ceil(Math.random()*255);
+    let g = Math.ceil(Math.random()*255);
+    let b = Math.ceil(Math.random()*255);
+    
+    const elemento = [...document.getElementsByClassName("conteudo")];
+    elemento.forEach( (el)=>{
+        el.style.backgroundColor = `rgb(${r},${g},${b})`;
+    } )
+
+   tmp = setTimeout(mudaCor, 1000);
+
 }
-});
 
+function alteraBanner1() {
+    
+    // let nr = Math.ceil(Math.random() *3);
+    const img1 = document.querySelector(".l-d > img");
+    const img2 = document.querySelector(".l-e > img");
+    img1.src = "./img/banner-lateral-1.png";
+    img2.src = "./img/banner-lateral-2.png";
+
+    setTimeout(alteraBanner2, 1000);
+
+}
+ 
+function alteraBanner2() {
+    
+    // let nr = Math.ceil(Math.random() *3);
+    const img1 = document.querySelector(".l-d > img");
+    const img2 = document.querySelector(".l-e > img");
+    img1.src = "./img/banner-lateral-2.png";
+    img2.src = "./img/banner-lateral-1.png";
+
+    setTimeout(alteraBanner3, 1000);
+
+}
+
+function alteraBanner3() {
+    
+    // let nr = Math.ceil(Math.random() *3);
+    const img1 = document.querySelector(".l-d > img");
+    const img2 = document.querySelector(".l-e > img");
+    img1.src = "./img/banner-lateral-1.png";
+    img2.src = "./img/banner-lateral-2.png";
+
+    setTimeout(alteraBanner1, 1000);
+
+}
+
+alteraBanner1();
